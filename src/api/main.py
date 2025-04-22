a8ae05d5e0b506a71f78752551aa16d01e6f0666
Rhaqim <anusiemj@gmail.com>
2025-04-22 15:20:58 +0100

    added cors policy for conexus

--- src/api/main.py
+++ src/api/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from api.routers import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(router.router, prefix="/api/v1", tags=["v1"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://conexus.degenerousdao.com", "https://conexus-test.degenerousdao.com", "http://localhost:4321"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
